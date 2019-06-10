7c8db322d3c445bca2a66ae30f77eea158217bff
Ernie-Wang <erniewangtw@gmail.com>
2019-06-10 16:34:34 +0800

    Feat: Add Lab3-1

---
+++ lab3/lab3-1/lab1.ino
@@ -0,0 +1,25 @@
+#include  <SoftwareSerial.h>
+SoftwareSerial BTSerial(10, 11); // RX 、 TX
+void setup()
+{
+  Serial.begin(9600);
+  BTSerial.begin(9600);  // HC-05 baudrate, match serial in order to print correctly
+  pinMode(5, OUTPUT);    // Set the led pin mode
+  digitalWrite(5, LOW);
+}
+void loop()
+{
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (BTSerial.available()) {
+    char recieve = BTSerial.read(); // get command from android via bluetooth
+    Serial.println(recieve);
+    if (recieve == '1') {           // If we set led to on
+      digitalWrite(13, HIGH);
+      BTSerial.write("LED ON\r\n");
+    }
+    else if (recieve == '0'){
+      digitalWrite(13, LOW);
+      BTSerial.write("LED OFF\r\n");
+    } else{}
+  }
+}
